37abf1020ff2628ecfd20da36aba2a3f146a451e
Anas <anaselnahef12@gmail.com>
2024-05-23 20:30:13 +0300

    add my protfolio projects

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -20,31 +20,196 @@
         <p class="text-5xl pt-24 text-white">Featured <span class="text-light-green">Projects</span></p>
 
         <div class="pt-16">
-            <div class="group">
-                <div class="relative w-11/12 rounded-lg overflow-hidden">
-                    <img
-                        src="https://via.placeholder.com/800x400"
-                        alt="Project 1"
-                        class="w-full h-96 object-cover group-hover:scale-105 transition-transform duration-300"
-                    />
+            <div class="space-y-20">
+                <div class="group">
+                    <div class="relative w-11/12 rounded-3xl overflow-hidden">
+                        <img
+                            src="~/assets/portfolio/streetwear.png
+                            "
+                            alt="streetwear e-commerce website"
+                            class="w-full h-min object-cover group-hover:scale-105 transition-transform duration-300"
+                        />
 
-                    <div class="absolute space-x-4 bottom-8 left-8">
-                        <button
-                            class="bg-white group-hover:bg-light-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
-                        >
-                            Figma
-                        </button>
-                        <button
-                            class="bg-white group-hover:bg-light-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
-                        >
-                            Figma
-                        </button>
+                        <div class="absolute space-x-4 bottom-8 left-8">
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                PHP
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Laravel
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                MYSQL
+                            </button>
+
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Livewire
+                            </button>
+
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                JQUERY
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                TailwindCSS
+                            </button>
+                        </div>
+                    </div>
+                    <div class="pt-8">
+                        <NuxtLink to="https://github.com/anas322/Streetwear-E-Commerce-Laravel" target="_blank">
+                            <p
+                                class="capitalize text-3xl group-hover:underline underline-offset-4 text-white transition-all duration-300"
+                            >
+                                Streetwear - Full-Stack E-commerce Website
+                            </p>
+                        </NuxtLink>
                     </div>
                 </div>
-                <div class="pt-8">
-                    <p class="text-3xl group-hover:underline underline-offset-4 text-white transition-all duration-300">
-                        Bureau - Architecture Studio Website
-                    </p>
+
+                <div class="group">
+                    <div class="relative w-11/12 rounded-3xl overflow-hidden">
+                        <img
+                            src="~/assets/portfolio/twitter.png
+                            "
+                            alt="Twitter clone website"
+                            class="w-full h-min object-cover group-hover:scale-105 transition-transform duration-300"
+                        />
+
+                        <div class="absolute space-x-4 bottom-8 left-8">
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                PHP
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Laravel
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                MYSQL
+                            </button>
+
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Nuxt.js 3
+                            </button>
+
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                TailwindCSS
+                            </button>
+                        </div>
+                    </div>
+                    <div class="pt-8">
+                        <NuxtLink to="https://github.com/anas322/Twitter-Clone-App" target="_blank">
+                            <p
+                                class="capitalize text-3xl group-hover:underline underline-offset-4 text-white transition-all duration-300"
+                            >
+                                Twitter Clone - Two seperate Apps one for front-end and one for back-end
+                            </p>
+                        </NuxtLink>
+                    </div>
+                </div>
+                <div class="group">
+                    <div class="relative w-11/12 rounded-3xl overflow-hidden">
+                        <img
+                            src="~/assets/portfolio/instgram.png
+                            "
+                            alt="intagram clone website"
+                            class="w-full h-min object-cover group-hover:scale-105 transition-transform duration-300"
+                        />
+
+                        <div class="absolute space-x-4 bottom-8 left-8">
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                PHP
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Laravel
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                inertia.js
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                MYSQL
+                            </button>
+
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                TailwindCSS
+                            </button>
+                        </div>
+                    </div>
+                    <div class="pt-8">
+                        <NuxtLink to="https://github.com/anas322/instagram-inspiration-app" target="_blank">
+                            <p
+                                class="capitalize text-3xl group-hover:underline underline-offset-4 text-white transition-all duration-300"
+                            >
+                                Instagram - Full-Stack Social Media Website (inspired by instagram)
+                            </p>
+                        </NuxtLink>
+                    </div>
+                </div>
+                <div class="group">
+                    <div class="relative w-11/12 rounded-3xl overflow-hidden">
+                        <img
+                            src="~/assets/portfolio/recipie.png
+                            "
+                            alt="recipe website"
+                            class="w-full h-min object-cover group-hover:scale-105 transition-transform duration-300"
+                        />
+
+                        <div class="absolute space-x-4 bottom-8 left-8">
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Vue.js
+                            </button>
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Vuex
+                            </button>
+
+                            <button
+                                class="bg-white group-hover:bg-dark-gray group-hover:text-white transition-colors duration-300 rounded-full py-2 px-6"
+                            >
+                                Vue Router
+                            </button>
+                        </div>
+                    </div>
+                    <div class="pt-8">
+                        <NuxtLink to="https://github.com/anas322/recipes-app" target="_blank">
+                            <p
+                                class="capitalize text-3xl group-hover:underline underline-offset-4 text-white transition-all duration-300"
+                            >
+                                Recipe APP - Single Page Application for adding and tracing recipes
+                            </p>
+                        </NuxtLink>
+                    </div>
                 </div>
             </div>
         </div>
