32545f451fe02a0ef470b2c0fa2a17778a1920a4
Anas <anaselnahef12@gmail.com>
2024-05-23 17:54:13 +0300

    add a custom scroll bar

--- app.vue
+++ app.vue
@@ -12,6 +12,12 @@ useHead({
 </script>
 
 <style>
+* {
+    font-family: "Inter", sans-serif;
+    font-weight: 200;
+}
+
+/* transition classes */
 .page-enter-active,
 .page-leave-active {
     transition: all 0.4s;
@@ -21,11 +27,30 @@ useHead({
     opacity: 0;
     filter: blur(1rem);
 }
+
+/* custom scrollbar */
+
+::-webkit-scrollbar {
+    width: 12px;
+    height: 20px;
+}
+
+::-webkit-scrollbar-track {
+    border-radius: 100vh;
+    background: #434343;
+}
+
+::-webkit-scrollbar-thumb {
+    background: #5c5b5b;
+    border-radius: 100vh;
+    border: 2px solid #6c6c6c;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #519172;
+}
+/*  */
 body {
     background-color: #1f1f1f;
-}
-* {
-    font-family: "Inter", sans-serif;
-    font-weight: 200;
 }
 </style>
